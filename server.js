66e19d53b431928c86ffa373a82955291f40d760
Shane Choi <jungmoney@Shanes-MacBook-Pro.local>
2025-03-13 22:29:14 -0400

    Prepare for Render deployment

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
